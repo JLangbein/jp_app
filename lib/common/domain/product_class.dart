e329423430b76b93c0d703e7cd3a13f0b308867e
Johannes Langbein <jllangbein@gmail.com>
2025-07-30 12:34:06 +0200

    started on ProductClass and ProductRepo, incluided icons as ttf

---
+++ lib/common/domain/product_class.dart
@@ -0,0 +1,18 @@
+class Product {
+  // attributes
+  final String name;
+  final String description;
+  final double price;
+  final String imageURL;
+  final int likes;
+  final double reviews;
+
+  Product({
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.imageURL,
+    required this.likes,
+    required this.reviews
+  });
+}
--- lib/main_app.dart
+++ lib/main_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jp_app/features/start/presentation/start_screen.dart';
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
@@ -7,9 +8,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: StartScreen(),
       ),
     );
   }
